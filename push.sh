585f62871cdb73aa1a15b4a0d664322f2b74e0b7
Do Van Phu <Do Van Phu@ABEOPC>
2020-04-14 10:18:01 +0900

    update

---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+find . -type f \( -iname "*.*" ! -path "./.git/*" ! -iname "checksum.dat" \) -exec bash -c "sha1sum '{}'" \; > checksum.dat
+git add -A
+git commit -m "update"
+git push
